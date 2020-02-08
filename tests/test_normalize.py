6247ad458b728e04c6bea34df27e82685ea31674
Andrew Scribner <ca.scribner@gmail.com>
2020-02-08 15:21:07 -0500

    Update normalize_image testing.  Test passes

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -12,42 +12,21 @@ from sqlalchemy.orm import Session
 from quick_redraw.data.metadata import Metadata
 from quick_redraw.data.metadata_db_session import global_init, create_session
 from quick_redraw.data.modelbase import SqlAlchemyBase
-
-
-# Fixtures built from here: https://gist.github.com/kissgyorgy/e2365f25a213de44b9a2
 from quick_redraw.etl.normalize import normalize_image
+
+# Fixtures inspired by: https://gist.github.com/kissgyorgy/e2365f25a213de44b9a2
+# Above patterns didn't quite work with how session __factory is shared.  Curious how I could improve my pattern as it
+# feels a little more closed minded than this would be
 
 
 FAKE_LABEL = 'fake_label'
 
-@pytest.fixture(scope='session')
-def db_engine():
-    return create_engine('sqlite://', echo=True)
-
 
 @pytest.fixture(scope='session')
-def db_tables(db_engine):
-    """
-    Builds all tables for this DB, then tears down after
-    """
-    SqlAlchemyBase.metadata.create_all(db_engine)
-    yield
-    SqlAlchemyBase.metadata.drop_all(db_engine)
+def db_init():
+    # global_init builds tables and populates session factory
+    global_init('', echo=True)
 
-
-@pytest.fixture
-def db_session(db_engine, db_tables):
-    # Not sure if we need to do this way or if we can do the same session-building methods from metadata_db_session.py
-    connection = db_engine.connect()
-    transaction = connection.begin()
-    session = Session(bind=connection)
-
-    yield session
-
-    # Teardown
-    session.close()
-    transaction.rollback()
-    connection.close()
 
 @pytest.fixture
 def dummy_image(tmpdir):
@@ -57,36 +36,47 @@ def dummy_image(tmpdir):
 
 
 @pytest.fixture
-def db_with_image(db_session, dummy_image):
+def db_with_image(dummy_image):
     m = Metadata()
     m.file_raw = dummy_image
     m.label = FAKE_LABEL
-    db_session.add(m)
-    db_session.commit()
-    m_objs = db_session.query(Metadata).all()
+    s = create_session()
+    s.add(m)
+    s.commit()
+    s.close()
+
+    # Not sure why, but if I don't close and reopen the session I get a thread error.
+    s = create_session()
+    m_objs = s.query(Metadata).all()
     print([str(m) for m in m_objs])
+    s.close()
 
-    return db_session
 
+def test_normalize_image_local(db_with_image, tmpdir, db_init):  # metadata_id, normalized_storage_location
+    """
+    Tests that normalize_image successfully loads the image from metadata_db, normalizes, and returns it to new store
 
-def test_normalize_image(db_with_image, tmpdir):  # metadata_id, normalized_storage_location
-    # global_init('')
-    # s = create_session()
-    # s.add(Metadata())
-    # s.commit()
-    # m_objs = s.query(Metadata).all()
-    # print(m_objs)
-    #
-    # assert False
+    Output metadata and image are both inspected
 
-    # Need to mock np.save because we don't want to actually save images
+    Future: Feels very scattered.  Break into a few tests?
+    """
+    # Verbose way to get m_id just in case we change the data creation routines.  This ensures there's a single record
+    s = create_session()
+    all_records = s.query(Metadata).all()
+    s.close()
+    assert len(all_records) == 1
+    assert all_records[0].file_normalized is None
 
-    normalize_image(0, tmpdir)
+    m_id = all_records[0].id
+    normalize_image(m_id, tmpdir)
 
-    normalized_image_filename = db_with_image.query(Metadata).all()[0].file_normalized
+    s = create_session()
+    all_records = s.query(Metadata).all()
+    s.close()
+    assert len(all_records) == 1
 
-    assert normalized_image_filename == os.join(tmpdir, f"{'fake_label'}_{0}.npy")
+    normalized_image_filename = all_records[0].file_normalized
+    assert normalized_image_filename == os.path.join(tmpdir, f"{FAKE_LABEL}_{m_id}.npy")
 
     normalized_image = np.load(normalized_image_filename)
-
     assert normalized_image.shape == (28, 28)
